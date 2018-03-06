29841dd9b879cef998790c7fdcb8a428ae60d20e
Souhail RAZZOUK <souhal.razzouk@hotmail.com>
2018-03-06 14:33:15 +0000

    gulpfile formatted

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,83 @@
-var gulp        = require('gulp'),
-    gutil       = require('gulp-util'),
-    sass        = require('gulp-sass'),
-    pug         = require('gulp-pug'),
-    imagemin    = require('gulp-imagemin'),
-    typescript  = require('gulp-typescript'),
-    tslint      = require('gulp-tslint')
-    tsl         = require("tslint"),
-    del         = require('del'),
-    merge       = require('merge2');
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  pug = require('gulp-pug'),
+  imagemin = require('gulp-imagemin'),
+  typescript = require('gulp-typescript'),
+  tslint = require('gulp-tslint'),
+  tsl = require("tslint"),
+  del = require('del'),
+  merge = require('merge2');
 
 var tsProject = typescript.createProject("./tsconfig.json");
 
-gulp.task('ts', function() {
+gulp.task('ts', function () {
   var tsResult = gulp.src('src/**/*.ts')
-                  .pipe( tsProject());    
+    .pipe(tsProject());
 
   return merge([
-      tsResult.dts.pipe(gulp.dest('dist/definitions/')),
-      tsResult.js.pipe(gulp.dest('dist/'))
-    ]);
+    tsResult.dts.pipe(gulp.dest('dist/definitions/')),
+    tsResult.js.pipe(gulp.dest('dist/'))
+  ]);
 });
 
-gulp.task('tslint', function() {
+gulp.task('tslint', function () {
   return gulp.src('src/**/*.ts')
-    .pipe( tslint({
+    .pipe(tslint({
       formatter: 'stylish'
     }))
-    .pipe( tslint.report({
+    .pipe(tslint.report({
       emitError: false
     }))
 })
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('src/**/*.scss')
-    .pipe( sass().on('error', sass.logError) )
-    .pipe( gulp.dest('dist/') );
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('pug', function() {
-return gulp.src(['src/**/*.pug', '!src/includes/**/*',])
-    .pipe( pug({ pretty: true }))
-    .pipe( gulp.dest('dist/'));
+gulp.task('pug', function () {
+  return gulp.src(['src/**/*.pug', '!src/includes/**/*', ])
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('images', function() {
-  return gulp.src(['src/**/*gif','src/**/*.jpg','src/**/*.png', 'src/**/*.ico'])
+gulp.task('images', function () {
+  return gulp.src(['src/**/*gif', 'src/**/*.jpg', 'src/**/*.png', 'src/**/*.ico'])
     .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
     }))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('clean', function () {
-	del(['dist/**/*', '!dist/', '!dist/bower_components', '!dist/bower_components/**/*'], {force: true}).then(paths => {
-    if(paths.length != 0){
+  del(['dist/**/*', '!dist/', '!dist/bower_components', '!dist/bower_components/**/*'], {
+    force: true
+  }).then(paths => {
+    if (paths.length != 0) {
       gutil.log('Files and folders that were deleted:\n', gutil.colors.orange(paths.join('\n')));
     }
   });
 });
 
 gulp.task('watch', function () {
-  
-  gulp.watch('src/**/*.pug',['pug']);
 
-  gulp.watch(['src/**/*gif','src/**/*.jpg','src/**/*.png','src/**/*.ico'],['images']);
+  gulp.watch('src/**/*.pug', ['pug']);
 
-  gulp.watch('src/**/*.scss',['sass']);
+  gulp.watch(['src/**/*gif', 'src/**/*.jpg', 'src/**/*.png', 'src/**/*.ico'], ['images']);
 
-  gulp.watch('src/**/*.ts',['ts', 'tslint']);
+  gulp.watch('src/**/*.scss', ['sass']);
+
+  gulp.watch('src/**/*.ts', ['ts', 'tslint']);
 
 });
 
-gulp.task('default', ['pug','sass', 'tslint','ts','images', 'watch']);
+gulp.task('default', ['pug', 'sass', 'tslint', 'ts', 'images', 'watch']);
 
-gulp.task('build', ['pug','sass', 'tslint','ts','images']);
+gulp.task('build', ['pug', 'sass', 'tslint', 'ts', 'images']);
